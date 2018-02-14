7ed04e8b43b3b7b844268ec805ccd2c9e03bece2
TYSS <TYSS@TYSS-4F-023>
2018-02-14 15:50:27 +0530

    Loggly and papertrail integration done... Not Working till now, Debugging...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { logglyLogger } from 'angular-loggly-logger';
+
+import { winston } from 'winston';
+import { Papertrail } from 'winston-papertrail';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -18,7 +23,21 @@ import { AboutComponent } from './about/about.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Papertrail],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+logglyLogger.config( '012b53fc-1cf6-427c-b329-5d5f588540e1', true );
+
+logglyLogger.sendMessage({ message: 'first log' });
+
+const winstonPapertrail: any = new winston.transports.Papertrail({ host: 'logs6.papertrailapp.com', port: 22117 });
+
+winstonPapertrail.on('error', function(err) {
+  // Handle, report, or silently ignore connection errors and failures
+});
+
+const logger: any = new winston.Logger({ transports: [winstonPapertrail] });
+
+logger.error('this is my message');
